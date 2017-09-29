8e5b89b5f6e6bcb18a3cd6fd6d49bbbb81bd2ee7
qishanquan <qisq_top@163.com>
2017-09-29 19:15:28 +0800

    edit rsync

--- rsync_work/push.bat
+++ rsync_work/push.bat
@@ -4,5 +4,3 @@ set rsyncDir=%projectDir%/rsync_work
 ::rsync -avz --progress /cygdrive/e/Wekan_work/wekan-dev_qisq/src admin@192.168.101.130::wekan_ftp
 
 rsync -avz --delete --progress --password-file=%rsyncDir%/rsyncd.secrets --exclude-from=%rsyncDir%/exclude_list.txt %projectDir%/src/ admin@10.10.6.56::wekan_ftp
-
-pause
